68180598d38baf76da3c53b02f51cc443ead825c
Kelly Miller <kellyannemiller@gmail.com>
2017-07-06 15:19:47 -0600

    complete walkthrough

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,31 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      authStatus: {
+        loggedIn: false,
+        username: '',
+        token: ''
+      },
       trains: []
     };
-
+    this.updateAuthStatus = this.updateAuthStatus.bind(this);
     this.updateTrains = this.updateTrains.bind(this);
+  }
+
+  componentDidMount() {
+    let token = localStorage.getItem('token');
+    let username = localStorage.getItem('username');
+
+    if (token && username) {
+     this.setState({
+       authStatus: {
+         loggedIn: true,
+         username,
+         token
+       }
+     })
+    }
+    this.fetchTrains();
   }
 
   fetchTrains() {
@@ -24,8 +45,9 @@ class App extends React.Component {
     });
   }
 
-  componentDidMount() {
-    this.fetchTrains();
+
+  updateAuthStatus(authStatus, redirect) {
+    this.setState({authStatus}, browserHistory.push(`/${redirect}/`));
   }
 
   updateTrains(trains) {
@@ -34,17 +56,23 @@ class App extends React.Component {
 
   render () {
     const { trains } = this.state;
-
+    const { authStatus } = this.state;
     return (
       <div>
       <h1>Big Metro City Choo-Choo Train Authority</h1>
-        {React.cloneElement(
-          this.props.children,
-          { 
-            trains,
-            updateTrains: this.updateTrains
-          }
-        )}
+      <Auth
+        username={authStatus.username}
+        updateAuthStatus={this.updateAuthStatus}
+      />
+      {React.cloneElement(
+        this.props.children,
+        {
+          authStatus,
+          updateAuthStatus: this.updateAuthStatus,
+          trains,
+          updateTrains: this.updateTrains
+        }
+      )}
        </div>
      )
    }
